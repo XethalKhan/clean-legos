10d617909777c5a25387bab397e648907d4d61a7
XethalKhan <bojovicpetar79614@gmail.com>
2022-10-16 18:53:06 +0200

    Removed address entity

--- packages/domain/domain-core/index.mjs
+++ packages/domain/domain-core/index.mjs
@@ -1,7 +1,5 @@
-import AddressImpl from './src/Address.js';
 import BaseEntityImpl from './src/BaseEntity.js';
 
-export const Address = AddressImpl;
 export const BaseEntity = BaseEntityImpl;
 
 export default BaseEntityImpl;
